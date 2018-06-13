f34a925334661f61a7353fd774d3450a4cc45382
oheydrew <drew@oheydrew.me>
2018-06-13 15:04:15 +1000

    Add basic frontend logic

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,12 @@ app.use(bodyParser.json());
 
 app.use('/', classmatesRouter);
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // run server
 app.listen( config.serverPort, () => {
     console.log( `Express server listening on port ${config.serverPort} 😀 ` );
@@ -24,5 +30,7 @@ app.listen( config.serverPort, () => {
   console.log( `*** Server Error! Details below: ***` );
   console.log( error );
 });
+
+
 
 // module.exports = mongoose;
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,52 @@
-// on click?? or link?
+let classmateContainer = document.getElementById('classmateContainer');
+
+function buildClassmate(classmate) {
+
+  const classmateList = document.createElement('ul');
+  const nameItem = document.createElement('li');
+  const companyItem = document.createElement('li');
+  const addressItem = document.createElement('li');
+
+  nameItem.textContent = `${classmate.firstName} ${classmate.lastName}`;
+  companyItem.textContent = classmate.company;
+  addressItem.textContent = classmate.address;
+
+  classmateList.appendChild(nameItem);
+  classmateList.appendChild(companyItem);
+  classmateList.appendChild(addressItem);
+
+  classmateContainer.appendChild(classmateList);
+}
+
+const classmatesUrl = 'http://localhost:3000/classmates';
+
+function fetchClassmates(url) {
+  fetch(url)
+  .then(response => { 
+    response.json();
+    console.log(response);
+  })
+  .then(classmates => {
+    console.log(classmates) 
+    listClassmates(classmates);
+  })
+  .catch(error => {
+    console.log(error.message);
+  });
+}
+
+function listClassmates(classmates) {
+  classmates.forEach(classmate => {
+    buildClassmate(classmate);
+  });
+}
+
+fetchClassmates(classmatesUrl);
+
+// classmate = {
+//               id: "5b208cc71296939a6d4fcddc",
+//               firstName: "gen",
+//               lastName: "mauger",
+//               company: "Code For Australia",
+//               address: "4 Brunswick Pl, Melbourne VIC 3000",
+//             }
